611bc6210ba1009ddc30094986821e51576c1e13
OM SETH <sethomni1564@gmail.com>
2026-07-04 10:53:57 +0530

    Time: 54 ms (74.7%), Space: 132.8 MB (73.4%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adjList(n+1);
-        for(auto &it:roads){
-            int u = it[0];
-            int v = it[1];
-            int d = it[2];
-            adjList[u].push_back({v,d});
-            adjList[v].push_back({u,d});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto &r : roads) {
+            int u = r[0], v = r[1], d = r[2];
+            adj[u].push_back({v, d});
+            adj[v].push_back({u, d});
         }
-        queue<pair<int,int>> q;
-        vector<int> reachMin(n+1,INT_MAX);
-        q.push({1,INT_MAX});
-        reachMin[1] = INT_MAX;
-        int minS = INT_MAX;
-    
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                auto [city , dist] = q.front();
-                q.pop();
-                minS = min(minS,dist);
-                for(auto &[nextC , nextD]:adjList[city]){
-                    if(reachMin[nextC]>nextD){
-                        q.push({nextC,nextD});
-                        reachMin[nextC] = nextD; 
-                    }
+
+        vector<bool> vis(n + 1, false);
+        queue<int> q;
+
+        q.push(1);
+        vis[1] = true;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &[next, dist] : adj[node]) {
+                ans = min(ans, dist);
+                if (!vis[next]) {
+                    vis[next] = true;
+                    q.push(next);
                 }
             }
         }
-        return minS;
+        return ans;
     }
 };
