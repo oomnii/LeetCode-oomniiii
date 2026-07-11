d9bda200ea09c8e934290879ac7fdf497ef43195
OM SETH <sethomni1564@gmail.com>
2026-07-12 01:15:33 +0530

    Add BrowserHistory class with linked list structure
    Implemented a doubly linked list to manage browser history with methods for visiting, going back, and going forward in history.

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -24,6 +24,58 @@ public:
     }
 };
 
+
+/** --------------------------------------- BY Doubly Linked List ---------------------------------------------------**/
+
+class BrowserHistory {
+public:
+    struct list{
+        string data = "";
+        list* next = NULL;
+        list* prev = NULL;
+    };
+    
+    list* curr = nullptr;
+
+    BrowserHistory(string homepage) {
+        list* entry = new list();
+        entry->data = homepage;
+        curr = entry;
+    }
+    
+    void clearForward(list* node) {
+        while (node) {
+            list* temp = node;
+            node = node->next;
+            delete temp;
+        }
+    }
+    
+    void visit(string url) {
+        clearForward(curr->next);
+        list* entry = new list();
+        entry->data = url;
+        entry->prev = curr;
+        curr->next = entry;
+        curr = entry;
+    }
+    
+    string back(int steps) {
+        while(curr->prev && steps){
+            curr = curr->prev;
+            steps--;
+        }
+        return curr->data;
+    }
+    
+    string forward(int steps) {
+        while(curr->next && steps){
+            curr = curr->next;
+            steps--;
+        }
+        return curr->data;
+    }
+};
 /**
  * Your BrowserHistory object will be instantiated and called as such:
  * BrowserHistory* obj = new BrowserHistory(homepage);
